a77838597b0f3742e1f2837fe58c2a0dac846147
fabriciodigennaro <slither33f@gmail.com>
2024-05-15 18:43:17 +0200

    Add Firstunique solutions

--- src/main/java/org/example/challenges/FirstUnique.java
+++ src/main/java/org/example/challenges/FirstUnique.java
@@ -1,8 +1,49 @@
 package org.example.challenges;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class FirstUnique {
 
-    public int solution(int[] input) {
-        return 0;
+//    given an array of integers, the solution needs to return the first unique integer found in the array
+//    a unique integer is an integer that doesn't repeat in the array
+
+    public Optional<Integer> solution(int[] input) {
+//        return solutionQuadratic(input);
+        return solutionLinear(input);
+    }
+
+    private Optional<Integer> solutionLinear(int[] input) {
+        Map<Integer, Integer> inputCounter = new LinkedHashMap<>();
+
+        for (int i = 0; i < input.length; i++) {
+            int targetNumber = input[i];
+            int previousValue = inputCounter.getOrDefault(targetNumber, 0);
+            inputCounter.put(targetNumber, previousValue + 1);
+        }
+        return inputCounter.entrySet().stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .findFirst();
+    }
+
+    private Optional<Integer> solutionQuadratic(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            int targetNumber = input[i];
+            boolean isRepeated = false;
+
+            for (int j = 0; j < input.length; j++) {
+                 if (targetNumber == input[j] && i != j) {
+                     isRepeated = true;
+                     break;
+                 }
+            }
+            if (!isRepeated) {
+                return Optional.of(targetNumber);
+            }
+        }
+//        throw new IllegalArgumentException();
+        return Optional.empty();
     }
 }
--- src/test/java/org/example/challenges/FirstUniqueTest.java
+++ src/test/java/org/example/challenges/FirstUniqueTest.java
@@ -1,5 +1,65 @@
 package org.example.challenges;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class FirstUniqueTest {
+
+    private final FirstUnique firstUnique = new FirstUnique();
+
+    @Test
+    void shouldFindFirstUnique() {
+        // Given
+         int[] input = {2, 2, 3, 5, 6, 3, 4};
+         int expected = 5;
+
+        // When
+        Optional<Integer> result = firstUnique.solution(input);
+
+        // Then
+        assertTrue(result.isPresent());
+        assertEquals(expected, result.get());
+    }
+
+    @Test
+    void shouldReturnEmptyOptionalWhenUniqueNotFound() {
+        // Given
+        int[] input = {2, 2, 3, 3, 6, 6, 4, 4};
+
+        // When
+        Optional<Integer> result = firstUnique.solution(input);
+
+        // Then
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    @Disabled
+    void shouldThrowAnExceptionWhenUniqueNotFound() {
+        // Given
+        int[] input = {2, 2, 3, 3, 6, 6, 4, 4};
+
+        // When
+        // Then
+        assertThrows(IllegalArgumentException.class, () -> firstUnique.solution(input));
+    }
+
+    @Test
+    @Disabled
+    void shouldReturnNullWhenUniqueNotFound() {
+        // Given
+        int[] input = {2, 2, 3, 3, 6, 6, 4, 4};
+
+        // When
+        Optional<Integer> result = firstUnique.solution(input);
+
+        // Then
+        assertNull(result);
+    }
+
 
 }
