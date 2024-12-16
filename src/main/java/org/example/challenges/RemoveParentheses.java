82184ae9f19b01067752c2cb26486993cde744cd
fabriciodigennaro <slither33f@gmail.com>
2024-12-16 14:36:26 +0100

    Add RemoveParentheses explanation

--- src/main/java/org/example/challenges/RemoveParentheses.java
+++ src/main/java/org/example/challenges/RemoveParentheses.java
@@ -1,5 +1,16 @@
 package org.example.challenges;
+/*
+https://www.codewars.com/kata/5f7c38eb54307c002a2b8cc8/train/java
 
+In this kata you are given a string for example:
+
+"example (unwanted thing)example"
+Your task is to remove everything inside the parentheses as well as the parentheses themselves.
+
+The example above would return:
+
+"example example"
+ */
 public class RemoveParentheses {
     // OPTION 1 REGEX and while:
 //    public static String removeParentheses(final String str) {
